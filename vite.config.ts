5dd34d69fa78a5bb8e4f40a94f3612724d94172a
Hhhkarimi <91493565+Hhhkarimi@users.noreply.github.com>
2026-07-30 09:33:42 +0330

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -7,10 +7,5 @@ export default defineConfig({
     sourcemap: false,
     target: "es2022",
     chunkSizeWarningLimit: 700,
-  },
-  test: {
-    environment: "jsdom",
-    setupFiles: ["./src/test/setup.ts"],
-    coverage: { reporter: ["text", "html"] },
   },
 });
